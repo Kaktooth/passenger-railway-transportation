57e2841ec8b38690d85072456f99035405fcf729
Lazerneek123 <Lazerneek123@yandex.ru>
2022-10-06 14:38:43 +0300

    Add dependencies for testing.

--- src/test/java/com/team/passengerrailwaytransportation/PassengerRailwayTransportationApplicationTests.java
+++ src/test/java/com/team/passengerrailwaytransportation/PassengerRailwayTransportationApplicationTests.java
@@ -1,7 +1,9 @@
 package com.team.passengerrailwaytransportation;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class PassengerRailwayTransportationApplicationTests {
