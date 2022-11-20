37bc38b472eaf41588176db66e9a7439c224cfee
Andrey Slynko <qkston22@gmail.com>
2022-11-20 17:53:34 +0200

    Refactor code for authorization

--- src/client/src/pages/Authorization/Authorization.js
+++ src/client/src/pages/Authorization/Authorization.js
@@ -1,12 +1,14 @@
 import { Alert, Button, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { SiTrainerroad } from "react-icons/si";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { loginUser } from "../../services/authorization";
 
 import styles from "./styles/Authorization.module.css";
 
 function Authorization() {
+	const navigate = useNavigate();
+
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
@@ -18,16 +20,35 @@ function Authorization() {
 	const onSignIn = async () => {
 		await loginUser({ email, password }).then(res => {
 			const { response } = res;
+
 			if (response.status === 400) {
 				const { email, password } = response.data.details;
 				setErrors({ ...errors, email: email || "", password: password || "" });
-			}
-
-			if (response.status === 401) {
+			} else if (response.status === 401 || response.status === 500) {
 				const { description } = response.data;
 				setErrors({ ...errors, description: description || "" });
+			} else if (response.status === 200) {
+				navigate("/stations");
+				localStorage.setItem("authorized", "1");
+				setErrors({ email: "", password: "", description: "" });
 			}
 		});
+	};
+
+	const renderTextField = (value, onChange, label, type, isRequired, error, helperText) => {
+		return (
+			<TextField
+				value={value}
+				onChange={event => onChange(event.target.value)}
+				className={styles.form_body_input}
+				label={label}
+				type={type}
+				variant="outlined"
+				required={isRequired}
+				error={!!error}
+				helperText={helperText}
+			/>
+		);
 	};
 
 	return (
@@ -38,27 +59,8 @@ function Authorization() {
 					<h4 className={styles.form_header_text}>Sign in</h4>
 				</div>
 				<div className={styles.form_body}>
-					<TextField
-						value={email}
-						onChange={event => setEmail(event.target.value)}
-						className={styles.form_body_input}
-						label="Email"
-						variant="outlined"
-						required
-						error={!!errors.email}
-						helperText={errors.email}
-					/>
-					<TextField
-						value={password}
-						onChange={event => setPassword(event.target.value)}
-						className={styles.form_body_input}
-						label="Password"
-						variant="outlined"
-						type="password"
-						required
-						error={!!errors.password}
-						helperText={errors.password}
-					/>
+					{renderTextField(email, setEmail, "Email", "email", true, !!errors.email, errors.email)}
+					{renderTextField(password, setPassword, "Password", "password", true, !!errors.password, errors.password)}
 					<NavLink className={styles.form_body_button} to={"/stations"}>
 						<Button
 							onClick={e => {
