22561351f7088cb88930fff345ed7a6e83fde1fc
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-11-20 21:14:28 +0200

    ADD NEW MIGRATION FOR WAGON_TYPES

--- src/main/resources/db/migration/V20220925133121__add_type_table.sql
+++ src/main/resources/db/migration/V20220925133121__add_type_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE wagon_types
 (
     id   UUID PRIMARY KEY,
     type VARCHAR NOT NULL,
-    seat_number INTEGER NOT NULL
+    seat_number INTEGER NOT NULL,
+    seat_price INTEGER NOT NULL
 );
--- src/main/resources/db/migration/V20221109150822__insert_wagon_types.sql
+++ src/main/resources/db/migration/V20221109150822__insert_wagon_types.sql
@@ -1,7 +1,7 @@
-INSERT INTO wagon_types (id, type, seat_number)
-VALUES ('9a72f7cb-e0a2-42d8-983c-6abcb7fa9620', 'compartment', 56);
-INSERT INTO wagon_types (id, type, seat_number)
-VALUES ('32a35722-448c-426f-9211-eef03fec96f9', 'open coach', 42);
-INSERT INTO wagon_types (id, type, seat_number)
-VALUES ('765f6195-a3f2-4ea6-8a99-043060164458', 'sleeping car', 52);
+INSERT INTO wagon_types (id, type, seat_number, seat_price)
+VALUES ('9a72f7cb-e0a2-42d8-983c-6abcb7fa9620', 'compartment', 56, 180);
+INSERT INTO wagon_types (id, type, seat_number, seat_price)
+VALUES ('32a35722-448c-426f-9211-eef03fec96f9', 'open coach', 42, 188);
+INSERT INTO wagon_types (id, type, seat_number, seat_price)
+VALUES ('765f6195-a3f2-4ea6-8a99-043060164458', 'sleeping car', 52, 200);
 
