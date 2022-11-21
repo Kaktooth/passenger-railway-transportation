aff3259fdcfe84d39f7bcde40d58f204b5052134
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-11-21 13:53:33 +0200

    BUG FIX

--- src/main/java/com/team/passengerrailwaytransportation/controller/LiqPayController.java
+++ src/main/java/com/team/passengerrailwaytransportation/controller/LiqPayController.java
@@ -4,6 +4,7 @@ import com.team.passengerrailwaytransportation.service.LiqPayApiService;
 import javax.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +18,8 @@ public class LiqPayController {
   }
 
   @PostMapping
-  public String buyTicket(@PathParam("amount") String amount,
-      @PathParam("description") String description, @PathParam("orderId") String orderId) {
+  public String buyTicket(@RequestParam("amount") String amount,
+      @RequestParam("description") String description, @RequestParam("orderId") String orderId) {
     return liqPayApiService.buyTicket(amount, description, orderId);
   }
 }
