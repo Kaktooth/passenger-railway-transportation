02e76d55f2a906912fdcbb62dfbdfd7f2790beea
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-11-21 13:26:24 +0200

    ADD SECURITY METHOD

--- src/main/java/com/team/passengerrailwaytransportation/controller/OperatorController.java
+++ src/main/java/com/team/passengerrailwaytransportation/controller/OperatorController.java
@@ -2,11 +2,11 @@ package com.team.passengerrailwaytransportation.controller;
 
 import com.team.passengerrailwaytransportation.service.OperatorService;
 import javax.mail.MessagingException;
-import javax.websocket.server.PathParam;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,11 +20,12 @@ public class OperatorController {
     this.operatorService = operatorService;
   }
 
-//  @PreAuthorize()
   @PostMapping("/sendMessage")
-  public HttpStatus sendMessage(@PathParam("message") String message,
-      @PathParam("subject") String subject, @PathParam("to") String to) throws MessagingException {
+  @PreAuthorize("hasRole('OPERATOR')")
+  public ResponseEntity<String> sendMessage(@RequestParam("message") String message,
+      @RequestParam("subject") String subject, @RequestParam("to") String to)
+      throws MessagingException {
     operatorService.sendMessage(message, subject, to);
-    return HttpStatus.OK;
+    return ResponseEntity.ok("Message sent.");
   }
 }
